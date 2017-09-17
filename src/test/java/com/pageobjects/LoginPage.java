a483e9dc5aa2ce41fd4d29810f8197998ef3946a
rsabat <roshnisabat76@gmail.com>
2017-09-17 16:20:43 +0530

    adde login page

---
+++ src/test/java/com/pageobjects/LoginPage.java
@@ -0,0 +1,10 @@
+package com.pageobjects;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("login page");
+	}
+
+}
